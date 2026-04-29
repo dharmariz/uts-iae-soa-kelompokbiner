8b87122f67dcb7021138a0281dffdc181cb12299
sweetyblue <vivianamaharani6@gmail.com>
2026-04-29 18:57:39 +0700

    feat: add processPayment consumer to OrderService

--- payment-service/routes/api.php
+++ payment-service/routes/api.php
@@ -3,4 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\PaymentController;
 
-Route::apiResource('payments', PaymentController::class);
+Route::get('/payments', [PaymentController::class, 'index']);
+Route::post('/payments', [PaymentController::class, 'store']);
+Route::post('/payments/process', [PaymentController::class, 'processPayment']); // ← harus di atas {id}
+Route::get('/payments/{id}', [PaymentController::class, 'show']);
+Route::put('/payments/{id}', [PaymentController::class, 'update']);
+Route::delete('/payments/{id}', [PaymentController::class, 'destroy']);
